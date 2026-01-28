4b1e1b587f81ba9e472ed0e870b7aaeddfc38c93
Yuri Pilschikov <yuri.nikolaevitch@gmail.com>
2026-01-28 19:54:55 +0300

    Table model looks like fixed

--- mysqltablemodel.cpp
+++ mysqltablemodel.cpp
@@ -12,7 +12,10 @@ QVariant MySqlTableModel::data(const QModelIndex &index, int role) const
     switch(role)
     {
     case Qt::DisplayRole:
-        return QSqlTableModel::data(index, Qt::DisplayRole);
+        //return QSqlTableModel::data(index, Qt::DisplayRole);
+        return QSqlTableModel::data(this->index(index.row(), 1), Qt::DisplayRole);
+    case Qt::ToolTipRole:
+        return QSqlTableModel::data(this->index(index.row(), 4));
     case Qt::DecorationRole:
         return QIcon(":/icons/violetIcon.png");
     }
